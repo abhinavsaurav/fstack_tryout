4bafa467630f8014cde1afd17d1c09b20e95b19b
abhinav <abhinavsaurav1@gmail.com>
2020-06-11 21:50:36 +0530

    fetching results

--- demo.py
+++ demo.py
@@ -23,10 +23,23 @@ cur.execute('insert into todos(id,description) values(%s,%s);',(1,"No Way"))
 
 dict1 ={'id':2,'descr':"hello world"}
 
-
-
 sql = 'insert into todos(id,description) values (%(id)s, %(descr)s);'
 cur.execute(sql, dict1)
+
+cur.execute('insert into todos(id,description) values(%s,%s);',(3,"No Way"))
+cur.execute('insert into todos(id,description) values(%s,%s);',(4,"OH Way"))
+cur.execute('insert into todos(id,description) values(%s,%s);',(5,"My Way"))
+
+cur.execute('select * from todos');
+result1=cur.fetchone()
+result2=cur.fetchmany(2)
+result=cur.fetchall()
+print("result1:", result1,"---result2---",result2,"--result3---",result)
+
+
+
+
+
 
 # commit, so it does the executions on the db and persists in the db
 conn.commit()
