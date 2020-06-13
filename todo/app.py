4e14e5bb10a766d24458a98804763bed63728f33
abhinav <abhinavsaurav1@gmail.com>
2020-06-14 01:26:10 +0530

    [feat,docs] MVC framework demo

--- todo/app.py
+++ todo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
@@ -19,14 +19,36 @@ class Todo(db.Model):
 #Don't forget this line too to make the database
 db.create_all()
 
+
+# Remember >>> methods <<<<<< 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # Also, the blank 2nd param is the default text passed if there is no data
+    description = request.form.get('description','')
+    
+    # Forgot the description = description field
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    
+    # We need to import redirect, url_for and request from flask -------------------[IMP]
+    # Also we are redirecting to the index function below so that it prints the data. 
+    # It optionally takes in a second argument which index function might take like redirect(url_for('index'),data)
+    # Also index might be taking a parameter data then like def index(data) 
+    return redirect(url_for('index'))
+    
 @app.route('/')
 def index():
     return render_template('index.html',data=Todo.query.all())
                            
-    #                        data=[{
+    # List of objects 
+    # 
+    # data=[{
     #     'description':"todo1"
     # },{
     #     'description':'todo2'
     # },{
     #     'description':'todos3'
     # }])
+    
+    
