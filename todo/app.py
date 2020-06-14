1253cb29aec27d3ae89c9ce260618eafd685386b
abhinav <abhinavsaurav1@gmail.com>
2020-06-14 17:37:59 +0530

    Before Migration

--- todo/app.py
+++ todo/app.py
@@ -1,6 +1,7 @@
 # Need to import jsonify for returning the json data
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys 
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost:5432/todoapp'
@@ -24,29 +25,35 @@ db.create_all()
 # Remember >>> methods <<<<<< 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    # Also, the blank 2nd param is the default text passed if there is no data
-    ''' description = request.form.get('description','')         '''
-    
-    # Updated--to JSON--
-    # We will receive a json body on ajax req which we set as the body and the param will be description
-    # So we are accessing the dictionary
-    description=request.get_json()['description']
-    
-    # Forgot the description = description field
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    
-    # We need to import redirect, url_for and request from flask -------------------[IMP]
-    # Also we are redirecting to the index function below so that it prints the data. 
-    # It optionally takes in a second argument which index function might take like redirect(url_for('index'),data)
-    # Also index might be taking a parameter data then like def index(data) 
-    # return redirect(url_for('index'))
-    
-    # Returning the data as a JSON value. ALso need to import jsonify from flask
-    return jsonify({
-        'description': todo.description
-    })
+    error=False
+    body={}
+    try:
+        # Also, the blank 2nd param is the default text passed if there is no data
+        ''' description = request.form.get('description','')         '''
+        
+        # Updated--to JSON--
+        # We will receive a json body on ajax req which we set as the body and the param will be description
+        # So we are accessing the dictionary
+        description=request.get_json()['description']
+        
+        # Forgot the description = description field
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        
+        body['description']=todo.description
+
+    except :
+        error=True
+        db.session.rollback()
+        print(sys.exc_info())
+        # Returning the data as a JSON value. ALso need to import jsonify from flask
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
     
 @app.route('/')
 def index():
