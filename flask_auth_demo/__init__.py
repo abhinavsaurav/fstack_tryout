3e7c6282350a0e55d5eb3e3a5158bf2fb12893c8
abhinav <abhinavsaurav1@gmail.com>
2020-07-09 00:52:23 +0530

    auth working on request

--- flask_auth_demo/__init__.py
+++ flask_auth_demo/__init__.py
@@ -1,12 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 
-def create_app(test_config=None):
-    app=Flask(__name__)
-    print("app running")
+
+
+app=Flask(__name__)
+print("app running")
+
+@app.route('/headers')
+def headers():
+    print("Hi Felicia!")
+    print()
+    if 'Authorization' not in request.headers:
+        abort(401)
     
-    @app.route('/headers')
-    def headers():
-        print("Hi Felicia!")
-        return "Bye Felicia!"
+    auth_token=request.headers['Authorization'].split(' ')
+    
+    if len(auth_token) != 2:
+        abort(401)
+    elif auth_token[0].lower() != 'bearer':
+        abort(401)
+    
+    return f"Bye Felicia! {auth_token[1]}"
+
     
     
