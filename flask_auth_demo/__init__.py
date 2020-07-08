74d648b90cb6d82bbdba37399e5afda4d0defced
abhinav <abhinavsaurav1@gmail.com>
2020-07-09 00:01:24 +0530

    Inital Auth commit

--- flask_auth_demo/__init__.py
+++ flask_auth_demo/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
 
-def create_app():
+def create_app(test_config=None):
     app=Flask(__name__)
+    print("app running")
+    
+    @app.route('/headers')
+    def headers():
+        print("Hi Felicia!")
+        return "Bye Felicia!"
     
     
