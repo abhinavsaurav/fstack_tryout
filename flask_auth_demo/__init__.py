f17fb27bfbcced039362a35ff03670e62efa8399
abhinav <abhinavsaurav1@gmail.com>
2020-07-09 16:15:03 +0530

    validation done

--- flask_auth_demo/__init__.py
+++ flask_auth_demo/__init__.py
@@ -1,20 +1,104 @@
 from flask import Flask, request, abort
+import json
 from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+AUTH0_DOMAIN = 'innocent.eu.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'Image'
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code  
 
 def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
     print("Hi Felicia!")
     print()
     if 'Authorization' not in request.headers:
-        abort(401)
-
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.1'
+        }, 401)
+        
     auth_token=request.headers['Authorization'].split(' ')
 
-    if len(auth_token) != 2:
-        abort(401)
-    elif auth_token[0].lower() != 'bearer':
-        abort(401)
+    if auth_token[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+    elif len(auth_token) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+    elif len(auth_token) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
     
+
     return auth_token[1]
+
+# Code provided by AUTH0 for verification 
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.4'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
 
 # creating decorator for getting the authorization token 
 # it makes thing more convinent now we don't have to pass any more value 
@@ -24,8 +108,12 @@ def requires_auth(f):
     def wrapper(*args, **kwargs):
         # calling the auth header
         jwt=get_token_auth_header()
+        try:
+            payload=verify_decode_jwt(jwt)
+        except:
+            abort(401)
         # passing the received value or headers as a parameter
-        return f(jwt,*args, **kwargs)
+        return f(payload,*args, **kwargs)
     return wrapper
 
 app=Flask(__name__)
@@ -33,10 +121,13 @@ print("app running")
 
 @app.route('/headers')
 @requires_auth
-def headers(jwt):
+def headers(payload):
     # the parameter can now be accessed above and then we can use it to print the values
-    print(jwt)
-    return f"Bye Felicia! {jwt}"
+    try:
+        print(payload)
+    except :
+        abort(401)
+    return f"Bye Felicia! {payload}"
 
     
     
